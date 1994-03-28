03bc50a2e6d3b56241bab7eba7727ee5ca23c6c1
William Lott <wlott@cs.cmu.edu>
1994-03-28 11:32:38 +0000

    Added debug-info.

--- mindy/comp/compile.h
+++ mindy/comp/compile.h
@@ -9,7 +9,7 @@
 *
 ***********************************************************************
 *
-* $Header: /home/housel/work/rcs/gd/src/mindy/comp/compile.h,v 1.1 1994/03/24 21:49:10 wlott Exp $
+* $Header: /home/housel/work/rcs/gd/src/mindy/comp/compile.h,v 1.2 1994/03/28 11:32:38 wlott Exp $
 *
 * This file does whatever.
 *
@@ -23,16 +23,46 @@ struct block {
     unsigned char bytes[0];
 };
 
+struct debug_info {
+    int line;
+    int bytes;
+    struct scope_info *scope;
+    struct debug_info *next;
+};
+
+struct var_info {
+    struct id *var;
+    boolean indirect;
+    boolean argument;
+    int offset;
+    struct var_info *next;
+};
+
+struct scope_info {
+    int handle;
+    int nvars;
+    struct var_info *vars;
+    struct var_info **vars_tail;
+    struct scope_info *outer;
+};
+
 struct component {
     struct literal *debug_name;
+    int frame_size;
+    int cur_line;
+    struct scope_info *cur_scope;
+    int cur_line_start;
+    int ndebug_infos;
+    struct debug_info *debug_info;
+    struct debug_info **debug_info_tail;
+    int nconstants;
+    struct constant *constants;
+    struct constant **constants_tail;
+    int bytes;
     struct block *blocks;
     struct block *cur_block;
     unsigned char *fill;
     unsigned char *end;
-    int bytes;
-    struct constant *constants;
-    struct constant **constants_tail;
-    int nconstants;
 };
 
 enum constant_kind {
