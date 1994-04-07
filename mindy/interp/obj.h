999212bc5c4e9e345301165843b48fab086ce996
Robert Stockton <rgs@cs.cmu.edu>
1994-04-07 18:29:36 +0000

    Allow arbitrarily large SAM cache.  Hit rate ~97%, ave 1.++ cells searched.

--- mindy/interp/obj.h
+++ mindy/interp/obj.h
@@ -9,7 +9,7 @@
 *
 ***********************************************************************
 *
-* $Header: /home/housel/work/rcs/gd/src/mindy/interp/obj.h,v 1.1 1994/03/24 21:49:39 wlott Exp $
+* $Header: /home/housel/work/rcs/gd/src/mindy/interp/obj.h,v 1.2 1994/04/07 18:29:36 rgs Exp $
 *
 * This file does whatever.
 *
@@ -19,3 +19,15 @@
 extern obj_t obj_ObjectClass;
 
 extern obj_t object_class(obj_t object);
+
+#ifdef __GNUC__
+#define object_class inline_object_class
+extern obj_t obj_IntegerClass;
+static __inline__ obj_t inline_object_class(obj_t object)
+{
+    if (((unsigned long)(object))&1)	/* obj_is_ptr(o) -- no mindy.h yet */
+	return obj_ptr(struct object *, object)->class;
+    else
+	return obj_IntegerClass;
+}
+#endif
