8528b803b758dd19a855702918ef5d08ea66ab78
William Lott <wlott@cs.cmu.edu>
1994-04-08 14:29:05 +0000

    Added support for dumping file parses.

--- mindy/comp/dump.c
+++ mindy/comp/dump.c
@@ -9,7 +9,7 @@
 *
 ***********************************************************************
 *
-* $Header: /home/housel/work/rcs/gd/src/mindy/comp/dump.c,v 1.4 1994/03/31 10:16:30 wlott Exp $
+* $Header: /home/housel/work/rcs/gd/src/mindy/comp/dump.c,v 1.5 1994/04/08 14:29:05 wlott Exp $
 *
 * This file does whatever.
 *
@@ -478,8 +478,7 @@ static void dump_constant(struct constant *c)
 
 /* Defconst and Defvar dumping. */
 
-static void dump_defconst_or_var(struct param_list *params,
-				 struct component *initializer)
+static void dump_defconst_or_var(struct param_list *params)
 {
     int count;
     struct param *p;
@@ -495,8 +494,6 @@ static void dump_defconst_or_var(struct param_list *params,
 	dump_symbol(p->id->symbol);
     if (params->rest_param)
 	dump_symbol(params->rest_param->symbol);
-
-    dump_component(initializer);
 }
 
 
@@ -526,17 +523,17 @@ static void dump_defnamespace(struct defnamespace_constituent *c,
 
 /* Interface to the output file dumper */
 
-void dump_setup_output(char *source, FILE *file)
+void dump_setup_output(char *source, FILE *file, char *what)
 {
     struct stat buf;
     struct timeval tv;
 
     File = file;
 
-    fprintf(File, "# compilation of %s\n", source);
+    fprintf(File, "# %s of %s\n", what, source);
     if (stat(source, &buf) >= 0)
 	fprintf(File, "# last modified on %s", ctime(&buf.st_mtime));
-    fprintf(File, "# compiled with the %s version of mindycomp\n", Version);
+    fprintf(File, "# produced with the %s version of mindycomp\n", Version);
     gettimeofday(&tv, NULL);
     fprintf(File, "# at %s", ctime(&tv.tv_sec));
 
@@ -592,13 +589,15 @@ void dump_defclass(struct id *name, struct slot_spec *slots,
 void dump_defconst(struct param_list *params, struct component *initializer)
 {
     dump_op(fop_DEFINE_CONSTANT);
-    dump_defconst_or_var(params, initializer);
+    dump_defconst_or_var(params);
+    dump_component(initializer);
 }
 
 void dump_defvar(struct param_list *params, struct component *initializer)
 {
     dump_op(fop_DEFINE_VARIABLE);
-    dump_defconst_or_var(params, initializer);
+    dump_defconst_or_var(params);
+    dump_component(initializer);
 }
 
 void dump_defmodule(struct defnamespace_constituent *c)
@@ -616,4 +615,433 @@ void dump_deflibrary(struct defnamespace_constituent *c)
 void dump_finalize_output(void)
 {
     dump_op(fop_DONE);
+}
+
+
+
+/* Stuff to dump program parses */
+
+static void dump_body(struct body *body);
+static void dump_expr(struct expr *expr);
+
+static void dump_id(struct id *id)
+{
+    dump_symbol(id->symbol);
+    dump_op(id->internal ? fop_TRUE : fop_FALSE);
+    dump_integer(id->line);
+}
+
+static void dump_param_list(struct param_list *params)
+{
+    struct param *p;
+    int nparams = 0;
+
+    for (p = params->required_params; p != NULL; p = p->next)
+	nparams++;
+    dump_integer(nparams);
+    for (p = params->required_params; p != NULL; p = p->next) {
+	dump_id(p->id);
+	if (p->type)
+	    dump_expr(p->type);
+	else
+	    dump_op(fop_FALSE);
+    }
+
+    if (params->next_param)
+	dump_id(params->next_param);
+    else
+	dump_op(fop_FALSE);
+
+    if (params->rest_param)
+	dump_id(params->rest_param);
+    else
+	dump_op(fop_FALSE);
+
+    if (params->allow_keys) {
+	struct keyword_param *k;
+	int nkeys = 0;
+
+	for (k = params->keyword_params; k != NULL; k = k->next)
+	    nkeys++;
+	dump_integer(nkeys);
+
+	for (k = params->keyword_params; k != NULL; k = k->next) {
+	    dump_symbol(k->keyword);
+	    dump_id(k->id);
+	    if (k->type)
+		dump_expr(k->type);
+	    else
+		dump_op(fop_FALSE);
+	    if (k->def)
+		dump_expr(k->def);
+	    else
+		dump_op(fop_FALSE);
+	}
+    }
+    else
+	dump_op(fop_FALSE);
+}
+
+static void dump_bindings(struct bindings *bindings)
+{
+    dump_param_list(bindings->params);
+    dump_expr(bindings->expr);
+}
+
+static void dump_rettypes(struct return_type_list *rettypes)
+{
+    struct return_type *r;
+    int nreq = 0;
+
+    if (rettypes != NULL) {
+	for (r = rettypes->req_types; r != NULL; r = r->next)
+	    nreq++;
+	dump_integer(nreq);
+	for (r = rettypes->req_types; r != NULL; r = r->next)
+	    if (r->type)
+		dump_expr(r->type);
+	    else
+		dump_op(fop_FALSE);
+	if (rettypes->rest_type)
+	    dump_expr(r->type);
+	else
+	    dump_op(fop_FALSE);
+    }
+    else
+	dump_op(fop_FALSE);
+}
+
+static void dump_plist(struct plist *plist)
+{
+    if (plist) {
+	struct property *p;
+	int nprops = 0;
+
+	for (p = plist->head; p != NULL; p = p->next)
+	    nprops++;
+	dump_integer(nprops);
+	for (p = plist->head; p != NULL; p = p->next) {
+	    dump_symbol(p->keyword);
+	    dump_expr(p->expr);
+	}
+    }
+    else
+	dump_integer(0);
+}
+
+static void dump_method_parse(struct method *method)
+{
+    if (method->name)
+	dump_id(method->name);
+    else
+	dump_op(fop_FALSE);
+    dump_param_list(method->params);
+    dump_rettypes(method->rettypes);
+    dump_body(method->body);
+}
+
+static void dump_varref_expr(struct varref_expr *expr)
+{
+    dump_op(fop_VARREF_EXPR);
+    dump_id(expr->var);
+}
+
+static void dump_literal_expr(struct literal_expr *expr)
+{
+    dump_op(fop_LITERAL_EXPR);
+    dump_literal(expr->lit);
+}
+
+static void dump_call_expr(struct call_expr *expr)
+{
+    struct argument *args;
+    int nargs = 0;
+
+    dump_op(fop_CALL_EXPR);
+    dump_expr(expr->func);
+    for (args = expr->args; args != NULL; args = args->next)
+	nargs++;
+    dump_integer(nargs);
+    for (args = expr->args; args != NULL; args = args->next)
+	dump_expr(args->expr);
+}
+
+static void dump_method_expr(struct method_expr *expr)
+{
+    dump_op(fop_METHOD_EXPR);
+    dump_method_parse(expr->method);
+}
+
+static void dump_dot_expr(struct dot_expr *expr)
+{
+    lose("dot expr made it though expand?");
+}
+
+static void dump_body_expr(struct body_expr *expr)
+{
+    dump_op(fop_BODY_EXPR);
+    dump_body(expr->body);
+}
+
+static void dump_block_expr(struct block_expr *expr)
+{
+    dump_op(fop_BLOCK_EXPR);
+    if (expr->exit_fun)
+	dump_id(expr->exit_fun);
+    else
+	dump_op(fop_FALSE);
+    dump_body(expr->body);
+    if (expr->inner)
+	lose("Dumping a block that still has exception clauses?");
+    if (expr->cleanup)
+	dump_body(expr->cleanup);
+    else
+	dump_op(fop_FALSE);
+    if (expr->outer)
+	lose("Dumping a block that still has exception clauses?");
+}
+
+static void dump_case_expr(struct case_expr *expr)
+{
+    lose("case made it though expand?");
+}
+
+static void dump_if_expr(struct if_expr *expr)
+{
+    dump_op(fop_IF_EXPR);
+    dump_expr(expr->cond);
+    dump_body(expr->consequent);
+    dump_body(expr->alternate);
+}
+
+static void dump_for_expr(struct for_expr *expr)
+{
+    lose("for made it though expand?");
+}
+
+static void dump_select_expr(struct select_expr *expr)
+{
+    lose("select made it though expand?");
+}
+
+static void dump_varset_expr(struct varset_expr *expr)
+{
+    dump_op(fop_VARSET_EXPR);
+    dump_id(expr->var);
+    dump_expr(expr->value);
+}
+
+static void dump_binop_series_expr(struct binop_series_expr *expr)
+{
+    lose("binop series made it though expand?");
+}
+
+static void dump_loop_expr(struct loop_expr *expr)
+{
+    dump_op(fop_LOOP_EXPR);
+    dump_body(expr->body);
+}
+
+static void dump_repeat_expr(struct repeat_expr *expr)
+{
+    dump_op(fop_REPEAT_EXPR);
+}
+
+static void dump_error_expr(struct expr *expr)
+{
+    lose("Called dump on a parse tree with errors?");
+}
+
+static void (*ExpressionDumpers[])() = {
+    dump_varref_expr, dump_literal_expr, dump_call_expr,
+    dump_method_expr, dump_dot_expr, dump_body_expr, dump_block_expr,
+    dump_case_expr, dump_if_expr, dump_for_expr, dump_select_expr,
+    dump_varset_expr, dump_binop_series_expr, dump_loop_expr,
+    dump_repeat_expr, dump_error_expr
+};
+
+static void dump_expr(struct expr *expr)
+{
+    (*ExpressionDumpers[(int)expr->kind])(expr);
+}
+
+static void dump_defconst_constituent(struct defconst_constituent *c)
+{
+    dump_op(fop_DEFINE_CONSTANT);
+    dump_bindings(c->bindings);
+}
+
+static void dump_defvar_constituent(struct defvar_constituent *c)
+{
+    dump_op(fop_DEFINE_VARIABLE);
+    dump_bindings(c->bindings);
+}
+
+static void dump_defmethod_constituent(struct defmethod_constituent *c)
+{
+    dump_op(fop_DEFINE_METHOD);
+    dump_method_parse(c->method);
+}
+
+static void dump_defgeneric_constituent(struct defgeneric_constituent *c)
+{
+    dump_op(fop_DEFINE_GENERIC);
+    dump_id(c->name);
+    dump_param_list(c->params);
+    dump_rettypes(c->rettypes);
+    dump_plist(c->plist);
+}
+
+static void dump_defclass_constituent(struct defclass_constituent *c)
+{
+    struct superclass *super;
+    struct slot_spec *slot;
+    struct keyword_spec *keyword;
+    struct inherited_spec *inherit;
+    int n;
+
+    dump_op(fop_DEFINE_CLASS);
+
+    n = 0;
+    for (super = c->supers; super != NULL; super = super->next)
+	n++;
+    dump_integer(n);
+    for (super = c->supers; super != NULL; super = super->next)
+	dump_expr(super->expr);
+    
+    n = 0;
+    for (slot = c->slots; slot != NULL; slot = slot->next)
+	n++;
+    dump_integer(n);
+    for (slot = c->slots; slot != NULL; slot = slot->next) {
+	switch (slot->alloc) {
+	  case alloc_INSTANCE:
+	    dump_symbol(symbol("instance"));
+	    break;
+	  case alloc_CLASS:
+	    dump_symbol(symbol("class"));
+	    break;
+	  case alloc_SUBCLASS:
+	    dump_symbol(symbol("subclass"));
+	    break;
+	  case alloc_CONSTANT:
+	    dump_symbol(symbol("constant"));
+	    break;
+	  case alloc_VIRTUAL:
+	    dump_symbol(symbol("virtual"));
+	    break;
+	  default:
+	    lose("strange slot allocation");
+	}
+	if (slot->name)
+	    dump_id(slot->name);
+	else
+	    dump_op(fop_FALSE);
+	if (slot->type)
+	    dump_expr(slot->type);
+	else
+	    dump_op(fop_FALSE);
+	dump_plist(slot->plist);
+    }
+
+    n = 0;
+    for (keyword = c->keywords; keyword != NULL; keyword = keyword->next)
+	n++;
+    dump_integer(n);
+    for (keyword = c->keywords; keyword != NULL; keyword = keyword->next) {
+	dump_symbol(keyword->keyword);
+	dump_plist(keyword->plist);
+    }
+
+    n = 0;
+    for (inherit = c->inherits; inherit != NULL; inherit = inherit->next)
+	n++;
+    dump_integer(n);
+    for (inherit = c->inherits; inherit != NULL; inherit = inherit->next) {
+	dump_id(inherit->name);
+	dump_plist(inherit->plist);
+    }
+}
+
+static void dump_expr_constituent(struct expr_constituent *c)
+{
+    dump_op(fop_EXPR_CONSTITUENT);
+    dump_expr(c->expr);
+}
+
+static void dump_local_constituent(struct local_constituent *c)
+{
+    struct method *m;
+    int nlocals = 0;
+
+    dump_op(fop_LOCAL_CONSTITUENT);
+    for (m = c->methods; m != NULL; m = m->next_local)
+	nlocals++;
+    dump_integer(nlocals);
+    for (m = c->methods; m != NULL; m = m->next_local)
+	dump_method_parse(m);
+    dump_body(c->body);
+}
+
+static void dump_handler_constituent(struct handler_constituent *c)
+{
+    dump_op(fop_HANDLER_CONSTITUENT);
+    dump_body(c->body);
+}
+
+static void dump_let_constituent(struct let_constituent *let)
+{
+    dump_op(fop_LET_CONSTITUENT);
+    dump_bindings(let->bindings);
+    dump_body(let->body);
+}
+
+static void dump_tlf_constituent(struct tlf_constituent *c)
+{
+    lose("top-level-form method inserted when parsing only?");
+}
+
+static void dump_error_constituent(struct constituent *c)
+{
+    lose("Called dump on a parse tree with errors?");
+}
+
+static void dump_defmodule_constituent(struct defnamespace_constituent *c)
+{
+    dump_op(fop_DEFINE_MODULE);
+    dump_defnamespace(c, TRUE);
+}
+
+static void dump_deflibrary_constituent(struct defnamespace_constituent *c)
+{
+    dump_op(fop_DEFINE_LIBRARY);
+    dump_defnamespace(c, FALSE);
+}
+
+
+static void (*DumpConstituents[])() = {
+    dump_defconst_constituent, dump_defvar_constituent,
+    dump_defmethod_constituent, dump_defgeneric_constituent,
+    dump_defclass_constituent, dump_expr_constituent,
+    dump_local_constituent, dump_handler_constituent,
+    dump_let_constituent, dump_tlf_constituent,
+    dump_error_constituent, dump_defmodule_constituent,
+    dump_deflibrary_constituent
+};
+
+static void dump_body(struct body *body)
+{
+    struct constituent *c;
+    int nconstits = 0;
+
+    for (c = body->head; c != NULL; c = c->next)
+	nconstits++;
+    dump_integer(nconstits);
+    for (c = body->head; c != NULL; c = c->next)
+	(*DumpConstituents[(int)c->kind])(c);
+}
+
+void dump_program(struct body *body)
+{
+    dump_body(body);
 }
