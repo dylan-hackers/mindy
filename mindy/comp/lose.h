72ee6e77f68b28d50d0479658f7ccb51325970dc
William Lott <wlott@cs.cmu.edu>
1994-04-09 14:10:14 +0000

    Initial revision

---
+++ mindy/comp/lose.h
@@ -0,0 +1,18 @@
+/**********************************************************************\
+*
+*  Copyright (C) 1994, Carnegie Mellon University
+*  All rights reserved.
+*
+*  This code was produced by the Gwydion Project at Carnegie Mellon
+*  University.  If you are interested in using this code, contact
+*  "Scott.Fahlman@cs.cmu.edu" (Internet).
+*
+***********************************************************************
+*
+* $Header: /home/housel/work/rcs/gd/src/mindy/comp/lose.h,v 1.1 1994/04/09 14:10:14 wlott Exp $
+*
+* This file does whatever.
+*
+\**********************************************************************/
+
+extern void lose(char *fmt, ...);
