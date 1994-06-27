eb4c181e251696e1cca755f0356794d10d3d6978
William Lott <wlott@cs.cmu.edu>
1994-06-27 16:32:48 +0000

    Fixed the boilerplate

--- mindy/interp/init.c
+++ mindy/interp/init.c
@@ -1,17 +1,31 @@
 /**********************************************************************\
 *
-*  Copyright (C) 1994, Carnegie Mellon University
+*  Copyright (c) 1994  Carnegie Mellon University
 *  All rights reserved.
-*
-*  This code was produced by the Gwydion Project at Carnegie Mellon
-*  University.  If you are interested in using this code, contact
-*  "Scott.Fahlman@cs.cmu.edu" (Internet).
+*  
+*  Use and copying of this software and preparation of derivative
+*  works based on this software are permitted, including commercial
+*  use, provided that the following conditions are observed:
+*  
+*  1. This copyright notice must be retained in full on any copies
+*     and on appropriate parts of any derivative works.
+*  2. Documentation (paper or online) accompanying any system that
+*     incorporates this software, or any part of it, must acknowledge
+*     the contribution of the Gwydion Project at Carnegie Mellon
+*     University.
+*  
+*  This software is made available "as is".  Neither the authors nor
+*  Carnegie Mellon University make any warranty about the software,
+*  its performance, or its conformity to any specification.
+*  
+*  Bug reports, questions, comments, and suggestions should be sent by
+*  E-mail to the Internet address "gwydion-bugs@cs.cmu.edu".
 *
 ***********************************************************************
 *
-* $Header: /home/housel/work/rcs/gd/src/mindy/interp/init.c,v 1.8 1994/04/20 02:37:29 wlott Exp $
+* $Header: /home/housel/work/rcs/gd/src/mindy/interp/init.c,v 1.9 1994/06/27 16:31:58 wlott Exp $
 *
-* This file does whatever.
+* This file initializes everything else.
 *
 \**********************************************************************/
 
--- mindy/interp/weak.c
+++ mindy/interp/weak.c
@@ -1,17 +1,31 @@
 /**********************************************************************\
 *
-*  Copyright (C) 1994, Carnegie Mellon University
+*  Copyright (c) 1994  Carnegie Mellon University
 *  All rights reserved.
-*
-*  This code was produced by the Gwydion Project at Carnegie Mellon
-*  University.  If you are interested in using this code, contact
-*  "Scott.Fahlman@cs.cmu.edu" (Internet).
+*  
+*  Use and copying of this software and preparation of derivative
+*  works based on this software are permitted, including commercial
+*  use, provided that the following conditions are observed:
+*  
+*  1. This copyright notice must be retained in full on any copies
+*     and on appropriate parts of any derivative works.
+*  2. Documentation (paper or online) accompanying any system that
+*     incorporates this software, or any part of it, must acknowledge
+*     the contribution of the Gwydion Project at Carnegie Mellon
+*     University.
+*  
+*  This software is made available "as is".  Neither the authors nor
+*  Carnegie Mellon University make any warranty about the software,
+*  its performance, or its conformity to any specification.
+*  
+*  Bug reports, questions, comments, and suggestions should be sent by
+*  E-mail to the Internet address "gwydion-bugs@cs.cmu.edu".
 *
 ***********************************************************************
 *
-* $Header: /home/housel/work/rcs/gd/src/mindy/interp/weak.c,v 1.4 1994/06/11 02:23:51 wlott Exp $
+* $Header: /home/housel/work/rcs/gd/src/mindy/interp/weak.c,v 1.5 1994/06/27 16:32:48 wlott Exp $
 *
-* This file does whatever.
+* This file implements weak pointers.
 *
 \**********************************************************************/
 
