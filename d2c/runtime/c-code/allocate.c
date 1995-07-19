a7115657ae45a9002d007e2ae22928885e5331e2
William Lott <wlott@cs.cmu.edu>
1995-07-19 23:45:05 +0000

    Use sbrk instead of malloc.

--- d2c/runtime/c-code/allocate.c
+++ d2c/runtime/c-code/allocate.c
@@ -1,8 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #include <runtime.h>
 
+static char *freeptr = NULL;
+static char *freeend = NULL;
+
 heapptr_t allocate(int bytes)
 {
-    return malloc(bytes);
+    heapptr_t res;
+
+    if (freeptr + bytes > freeend) {
+	freeptr = sbrk(1024*1024);
+	freeend = freeptr + 1024*1024;
+    }
+
+    res = (heapptr_t)freeptr;
+    freeptr += (bytes + 7) & ~7;
+
+    return res;
 }
