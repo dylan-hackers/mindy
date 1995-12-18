68fffb3a97ba5004e6eaefcc935c97ed3482bd11
William Lott <wlott@cs.cmu.edu>
1995-12-18 05:12:08 +0000

    Added not_reached().

--- d2c/runtime/c-code/main.c
+++ d2c/runtime/c-code/main.c
@@ -1,8 +1,14 @@
-/* $Header: /home/housel/work/rcs/gd/src/d2c/runtime/c-code/main.c,v 1.1 1995/11/06 17:22:35 wlott Exp $ */
+/* $Header: /home/housel/work/rcs/gd/src/d2c/runtime/c-code/main.c,v 1.2 1995/12/18 05:12:08 wlott Exp $ */
 
 #include <stdlib.h>
 
 #include <runtime.h>
+
+void not_reached(void)
+{
+    fprintf(stderr, "entered a branch that supposedly never could be.\n");
+    abort();
+}
 
 void main(int argc, char *argv[])
 {
