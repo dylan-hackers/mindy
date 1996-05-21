8374b5f091a4d197b4250789c6196c419e650788
Nick Kramer <nkramer@cs.cmu.edu>
1996-05-21 22:18:53 +0000

    Changed it for VC++ 4.x

--- mindy/MS-Windows/prepare-for-vc.bat
+++ mindy/MS-Windows/prepare-for-vc.bat
@@ -1,16 +1,17 @@
 @echo off
 
-rem Does miscellaneous stuff to the NT distribution to prepare it for
+rem Does miscellaneous stuff to the Windows distribution to prepare it for
 rem Visual C++
 
 echo Converting LF to CR-LF in Mindy.mak
-d:\winntunix\gnu\usr\local\bin\perl -ne "print $_;" Mindy.mak > temp.mak
+d:\gnu\usr\local\bin\perl -ne "print $_;" Mindy.mak > temp.mak
 del Mindy.mak
 ren temp.mak Mindy.mak
 
-mkdir Released
-mkdir Debug
-mkdir Released\Objects
-mkdir Debug\Objects
+mkdir Compiler
+mkdir Interpretter
+mkdir Compiler\Release
+mkdir Interpretter\Release
+
 mkdir bin
 mkdir lib
