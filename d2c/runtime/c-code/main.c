b76439fe927f1a4aeafb29c6b0a0973e24b43d35
Robert Stockton <rgs@cs.cmu.edu>
1996-06-24 20:07:09 +0000

    Added various debugguing support routines to a file which is sure to be linked in.

--- d2c/runtime/c-code/main.c
+++ d2c/runtime/c-code/main.c
@@ -1,4 +1,4 @@
-/* $Header: /home/housel/work/rcs/gd/src/d2c/runtime/c-code/main.c,v 1.4 1996/02/09 17:15:18 wlott Exp $ */
+/* $Header: /home/housel/work/rcs/gd/src/d2c/runtime/c-code/main.c,v 1.5 1996/06/24 20:07:09 rgs Exp $ */
 
 #include <stdlib.h>
 
@@ -19,3 +19,126 @@ void main(int argc, char *argv[])
     /* Run all the top level initializations. */
     inits(sp, argc, argv);
 }
+
+
+/* GDB support routines */
+/*   The following routines are included to allow easier debugging via the */
+/*   GDB debugger.  There has been very little attempt to make them pretty */
+/*   or even terribly stable.  However, they have sufficient utility at    */
+/*   present to (hopefully) justify a bit of hackishness.                  */
+
+#define MAX_GDB_RECURSION 20 
+#define MAX_RESULTS_COUNT 20
+#define GDB_STACK_SIZE 100000
+
+descriptor_t *gdb_stack_stack[MAX_GDB_RECURSION];
+descriptor_t gdb_result_stack[MAX_RESULTS_COUNT];
+int gdb_stack_stack_index = 0;
+int gdb_stack_index = 0;
+
+extern descriptor_t dylan_literal_3;
+extern descriptor_t dylan_cls_byte_string;
+extern void dylan_gdb_print_object_main();
+
+void string_arg (char *arg) {
+  descriptor_t tmp;
+  descriptor_t *stack = gdb_stack_stack[gdb_stack_stack_index];
+  void **dylan_str;
+
+  if (stack == 0)
+    stack = gdb_stack_stack[gdb_stack_stack_index]
+      = (descriptor_t *) malloc(GDB_STACK_SIZE);
+
+  dylan_str = (void **)
+    GC_malloc(sizeof(struct heapobj *) + sizeof(long) + strlen(arg));
+  dylan_str[0] = (void *)(dylan_cls_byte_string.heapptr);
+  dylan_str[1] = (void *)strlen(arg);
+  strcpy((char *)(&dylan_str[2]), arg);
+
+  tmp.heapptr = (struct heapobj *)dylan_str;
+  tmp.dataword.l = 0;
+  stack[gdb_stack_index++] = tmp;
+}
+
+void int_arg (int arg) {
+  descriptor_t tmp;
+  descriptor_t *stack = gdb_stack_stack[gdb_stack_stack_index];
+
+  if (stack == 0)
+   stack = gdb_stack_stack[gdb_stack_stack_index]
+      = (descriptor_t *) malloc(GDB_STACK_SIZE);
+
+  /* HACK -- wired in name for <integer> proxy */
+  tmp.heapptr = dylan_literal_3.heapptr;
+  tmp.dataword.l = arg;
+  stack[gdb_stack_index++] = tmp;
+}
+
+void heap_arg (struct heapobj *arg) {
+  descriptor_t tmp;
+  descriptor_t *stack = gdb_stack_stack[gdb_stack_stack_index];
+
+  if (stack == 0)
+   stack = gdb_stack_stack[gdb_stack_stack_index]
+      = (descriptor_t *) malloc(GDB_STACK_SIZE);
+
+  tmp.heapptr = arg;
+  tmp.dataword.l = 0;
+  stack[gdb_stack_index++] = tmp;
+}
+
+void desc_arg (descriptor_t arg) {
+  descriptor_t *stack = gdb_stack_stack[gdb_stack_stack_index];
+
+  if (stack == 0)
+   stack = gdb_stack_stack[gdb_stack_stack_index]
+      = (descriptor_t *) malloc(GDB_STACK_SIZE);
+
+  stack[gdb_stack_index++] = arg;
+}
+
+#define GENERAL_ENTRY(func) \
+    ((entry_t)SLOT(func, void *, 8))
+
+int gdb_invoke_function (descriptor_t fun, int count)
+{
+  descriptor_t *stack = gdb_stack_stack[gdb_stack_stack_index];
+  descriptor_t *result, *newstack;
+  int i, result_count;
+
+  if (stack == 0)
+    stack = gdb_stack_stack[gdb_stack_stack_index]
+      = (descriptor_t *) malloc(GDB_STACK_SIZE);
+  gdb_stack_stack_index++;
+  gdb_stack_index = 0;
+
+  result = GENERAL_ENTRY(fun.heapptr)(stack + count, fun.heapptr, count);
+  newstack = gdb_stack_stack[gdb_stack_stack_index];
+  if (newstack == 0)
+    newstack = gdb_stack_stack[gdb_stack_stack_index]
+      = (descriptor_t *) malloc(GDB_STACK_SIZE);
+  gdb_stack_stack_index++;
+  gdb_stack_index = 0;
+  result_count = result - stack;
+  for (i = 0 ; i < result_count; i++)
+    gdb_result_stack[i] = stack[i];
+  fflush(stdout);
+  fflush(stderr);
+
+  --gdb_stack_stack_index;
+  return result_count;
+}
+
+gdb_print_heapobj (struct heapobj *obj)
+{
+  descriptor_t tmp;
+  tmp.heapptr = obj;
+  tmp.dataword.l = 0;
+  dylan_gdb_print_object_main((descriptor_t *)GC_malloc(10000), tmp);
+}
+
+gdb_print_genobj (descriptor_t obj)
+{
+  dylan_gdb_print_object_main((descriptor_t *)GC_malloc(10000), obj);
+}
+
