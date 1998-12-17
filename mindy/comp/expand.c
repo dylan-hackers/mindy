c9b9bb61f59a374d497746808181880b96b0bc99
Jeff Dubrule <igor@pobox.com>
1998-12-17 11:10:14 +0000

    'expand_rettypes' and 'expand_plist' are defined, but are not actually used anywhere.  Removed.

--- mindy/comp/expand.c
+++ mindy/comp/expand.c
@@ -23,7 +23,7 @@
 *
 ***********************************************************************
 *
-* $Header: /scm/cvs/src/mindy/comp/expand.c,v 1.2 1998/08/10 15:05:47 housel Exp $
+* $Header: /scm/cvs/src/mindy/comp/expand.c,v 1.3 1998/12/17 11:10:14 igor Exp $
 *
 * This file does source-to-source expansions.
 *
@@ -106,17 +106,6 @@ static void expand_bindings(struct bindings *bindings)
     expand_expr(&bindings->expr);
 }
 
-static void expand_rettypes(struct return_type_list *rettypes)
-{
-    struct return_type *r;
-
-    for (r = rettypes->req_types; r != NULL; r = r->next)
-	if (r->type)
-	    expand_expr(&r->type);
-    if (rettypes->rest_type)
-	expand_expr(&rettypes->rest_type);
-}
-
 static void bind_rettypes(struct body *body,
 			  struct return_type_list *rettypes)
 {
@@ -154,16 +143,6 @@ static void bind_rettypes(struct body *body,
 	bind_temp(body, id(rettypes->rest_temp),
 		  make_function_call(make_varref(id(ctype)), args));
 	rettypes->rest_temp_varref = make_varref(id(rettypes->rest_temp));
-    }
-}
-
-static void expand_plist(struct plist *plist)
-{
-    if (plist) {
-	struct property *p;
-
-	for (p = plist->head; p != NULL; p = p->next)
-	    expand_expr(&p->expr);
     }
 }
 
