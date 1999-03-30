a2904c8b2626f1638e9a8eb205cf45b9800e242b
Eric Kidd <git@randomhacks.net>
1999-03-30 05:54:11 +0000

    * Commented out unsupported declarations.  * Added new declarations to test declarations, pointers and typedefs.

--- tools/pidgin/headers/ansi-test.h
+++ tools/pidgin/headers/ansi-test.h
@@ -1,6 +1,15 @@
+/*
+Let's not confuse the poor thing for the time being.
+
 extern char * foo;
-
 extern char bar;
-
 extern int foobar(int a, int b);
-
+*/
+extern char d1;
+extern char * d2;
+extern char *** d3;
+extern signed long long d4;
+extern unsigned int d5, *d6, **d7, ***d8, *****d9;
+typedef int t1;
+typedef t1 *t2;
+typedef int **t3;
