1652d779a7838069e59d8a72cd4c6f60af6171bd
Rob Myers <rob@robmyers.org>
1999-11-07 13:57:01 +0000

    Commented some sections more informatively.

--- mindy/Macintosh/MacSource/Interpreter/self-runner.c
+++ mindy/Macintosh/MacSource/Interpreter/self-runner.c
@@ -1,6 +1,8 @@
 // self-runner.c
 
-// Just turn on the self-runner define & inlcude the main runner code
+// Just turn on the self-runner define & include the main runner code
+
+// Automatically shows a CLI dialog if command is held down or a 'CLI?' 128 resource is present
 
 #define SELF_RUNNER
 
