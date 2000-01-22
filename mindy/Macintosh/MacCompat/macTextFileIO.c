d50e080f8e2f50c796caea1c2724dfd2578a65c2
Rob Myers <rob@robmyers.org>
2000-01-22 23:20:43 +0000

    Made more robust. Added asserts. Treats .du files as binary.

--- mindy/Macintosh/MacCompat/macTextFileIO.c
+++ mindy/Macintosh/MacCompat/macTextFileIO.c
@@ -7,7 +7,7 @@
 // includes
 
 #include <console.h>
-#include <errno.h>
+#include <sys/errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,9 +22,12 @@
 #include <Script.h>
 #include <MacMemory.h>
 
-#include <abort_exit.h>
 #include <fcntl.h>
-#include <path2fss.h>
+#include <stat.h>
+
+#include <sys/errno.h>
+
+#include<assert.h>
 
 char * LIBDIR;
 
@@ -55,16 +58,13 @@ char * LIBDIR;
 #	define FALSE	0
 #endif
 
-#ifdef MINDY_DEBUGGER
-	#include "std-c.h"
-	#include "mindy.h"
-	#include "gc.h"
-	#include "bool.h"
-	#include "class.h"
-	#include "str.h"
-	#include "thread.h"
-	#include "vec.h"
-#endif
+#undef write
+
+//#ifdef __D2C
+//	#define ALLOCATE_CONVERSION_BUFFER(a)	(char *)GC_malloc_uncollectable(a)
+//#else
+//	#define ALLOCATE_CONVERSION_BUFFER(a) 	NewPtr(a)
+//#endif
 
 // prototypes
 
@@ -73,12 +73,11 @@ int MacClose( int fd );
 int MacCreat(const char *path, mode_t mode);
 int MacRead(int fd, char *buf, int count);
 int MacWrite(int fd, const char *buf, int count);
-OSErr ReallyWrite( int fd, Ptr buffer, long * amount );
-int FindUnusedFileStatus( void );
-long OpenApplicationAsFile( void );
-Boolean IsDUFile( Str255 name );
+short OpenApplicationAsFile( void );
+Boolean IsDUFile( const char * name );
+Boolean IsDBCFile( const char * name );
+void SetFileText( const char * path, int fd, int flags );
 void FileError( int err );
-
 
 // static prototypes
 
@@ -93,30 +92,19 @@ static void r2n( char * buffer, int count);
 
 // 0 is text as \r must be \n . 1 and 2 are binary as they expect \n
 
-typedef struct
-{
-	long 	fileNum;
-	Boolean	isText;
-} FileStatus;
-
-FileStatus __fileIsText[ MAX_FILES ] =
-{
-	{ -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, 
-	{ -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE },
-	{ -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE },
-	{ -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE },
-	{ -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE },
-	{ -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE },
-	{ -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE },
-	{ -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE }, { -1, FALSE },
-};
+int __fileIsBinary[ MAX_FILES ] = {}; // Init to 0's
 
 
 // static globals
 
 static char * conversionBuffer = NULL;
 
-static Boolean gD2c = FALSE;
+static Boolean gD2c =
+						#ifdef __D2C
+							TRUE;
+						#else
+							FALSE;
+						#endif
 
 
 // functions
@@ -127,87 +115,27 @@ static Boolean gD2c = FALSE;
 
 int MacOpen( const char *path, int flags, int mode )
 {
-	OSErr err;
-	FSSpec fileSpec;
-	int fileStatusRecord;
-	short fd;
-	char permission;
+	int fd;
 	
-	// Open data fork as file?
-	if( strcmp( path, "The Application File            " ) == 0 )	// 0 = equality
+	if( strcmp( path, "The Application File            " ) )	// 0 == match, so check for fail
 	{
-		return OpenApplicationAsFile();
-	}
-	
-	// Make FSSpec
-	err = __path2fss( path, &fileSpec );
-	
-	// Set flags
-	if( flags & O_RDONLY )		
-		permission = fsRdPerm; 		
-	else if( (flags & O_WRONLY) || (flags & O_RDWR ) )		
-		permission = fsRdWrPerm; 
-	else
-		permission = fsCurPerm;	
-
-	// open data fork
-	err = FSpOpenDF( &fileSpec, permission, &fd);
-	
-	// If there's an error
-	if (err) 
-	{
-		// File not found, but might be creatable
-		if (err == fnfErr && flags & O_CREAT) 
+		if( IsDUFile( path ) )
 		{
-			err = FSpCreate( &fileSpec, 'MPS ', 'TEXT', smSystemScript );
-			//if (!err)	//-
-			//	err = FSpTouchFolder( &fileSpec );
-			if (!err || err == dupFNErr )
-				err = FSpOpenDF( &fileSpec, permission, &fd );
+			flags |= O_BINARY;
 		}
-		else
-		{
-			fd =  -1;
-			goto finally;
-		}
-	}
-	else if (flags & O_EXCL) 
-	{
-		FSClose(fd);
-		fd = -1;
-		goto finally;
-	}
 		
-	// Truncate?	
-	if( flags & O_TRUNC )
-		SetEOF( fd, 0 );
-	
-	
-	// finally
-	
-	finally:
-		
-	if( fd > 0 )									// If opened ok
-	{
-		Boolean b; //-
-		fileStatusRecord = FindUnusedFileStatus();
-		__fileIsText[ fileStatusRecord ].fileNum = fd;
-		// hack for .du files
-		if( gD2c && IsDUFile( fileSpec.name ) )
-			__fileIsText[ fileStatusRecord ].isText = FALSE;
-		else
-			__fileIsText[ fileStatusRecord ].isText = !(flags & O_BINARY );	// Set flag
-		
-		b = __fileIsText[ fileStatusRecord ].isText;
+		fd = open( path, flags );
 	}
 	else
-	{	
-		fd = -1;
+	{
+		fd = (int)OpenApplicationAsFile();
 	}
 	
-
-	FileError( err );
-	return fd;										// Return the fd
+	FileError( fd );
+	if( fd >= 0 )
+		SetFileText( path, fd, flags );
+	
+	return fd;
 }
 
 // MacClose
@@ -215,40 +143,19 @@ int MacOpen( const char *path, int flags, int mode )
 
 int MacClose( int fd )
 {
-	int 	fileNum = 0;
-	short 	vRefNum;
-	long 	filePos;
-	OSErr 	err;
+	int fileNum = 0;
 	
-	// Reset the data structure
-	while( fileNum < MAX_FILES )
+	assert( fd > 2 );	// Closing the std streams would be bad
+	
+	for( fileNum = 0; fileNum < MAX_FILES; fileNum++ )
 	{
-		if( __fileIsText[ fileNum ].fileNum == fd )
+		if( __fileIsBinary[ fileNum ] == fd )
 		{
-			__fileIsText[ fileNum ].fileNum = -1;
+			__fileIsBinary[ fileNum ] = 0;
 		}
-		fileNum++;
 	}
-	
-	// If it's the console, return OK
-	if ( fd < 3 && fd >= 0 )
-	{
-		return 0;
-	}
-	
-	// Truncate file to current position
-	/*err = GetFPos( fd, &filePos );
-	if( err == noErr )
-		err = SetEOF( fd, filePos );*/
-		
-	// Close the file and flush the volume to make sure EOF gets written	
-	GetVRefNum( fd, &vRefNum );
-	err = FSClose( fd );
-	FlushVol( NULL, vRefNum );
-	
-	// Return some sort of error code
-	FileError( err );
-	return (err == noErr ? 0 : -1 );
+
+	return close( fd );
 }
 
 // creat
@@ -257,13 +164,10 @@ int MacClose( int fd )
 
 int MacCreat(const char *path, mode_t mode)
 {
-	int		openMode = O_WRONLY | O_CREAT | O_TRUNC;
-	int 	fd;
-	
-	if (mode & O_BINARY)
-		openMode |= O_BINARY;
-
-	fd = MacOpen( path, openMode, 0);
+    int fd = creat( path, mode );
+    FileError( fd );
+    if( fd >= 0 )
+		SetFileText( path, fd, mode );
 	
 	return fd;
 }
@@ -274,50 +178,26 @@ int MacCreat(const char *path, mode_t mode)
 
 int MacRead(int fd, char *buf, int count)
 {
-	IOParam			param;
-	OSErr			theError;
-	int				readCount;
+	int result;
+	Boolean isText;
 	
-	if( conversionBuffer == NULL )
-	{
-		conversionBuffer = NewPtr( WRITE_BUFFER_SIZE );
-		if( conversionBuffer == NULL || MemError() )
-		{
-			return -1;
-		}
-	}
-
-	if ((fd == 0)) 												// If it's 0/stdin
-	{
-		if (InstallConsole(fd) == 0)							// Read from the console 
-		{
-			__console_exit = RemoveConsole;
-			fflush(stdout);
-			readCount =  ReadCharsFromConsole((char *)buf, count);
-			r2n( buf, readCount );			// It's text, so convert
-			return readCount;
-		} 
-		else
-			return -1;
-
-	}
-
-	readCount = count;
+	assert( fd >= 0 );
+	assert( buf != NULL );
+	assert(  count > 0 );
 	
-	theError = FSRead( fd, &readCount, buf );
-
-	if (theError != noErr && theError != eofErr)				// Unexpected theErrorors?
+	result = read( fd, buf, count );
+	FileError( result );
+	isText = FileIsText(fd);
+	
+	assert( (isText == FALSE) || (isText == TRUE) );
+	
+	if( isText && (result > 0) )
 	{
-		FileError( theError );										// Set the errno
-		readCount = -1;											// Return the error or count
-	}	
-	else														// Otherwise
-	{
-		if( FileIsText( fd ) )									// If the file's text
-			r2n( buf, readCount );								// Convert it
+		r2n( buf, count );
+		return result;
 	}
 	
-	return readCount;											// Return the count
+	return result;
 }
 
 // write
@@ -325,101 +205,31 @@ int MacRead(int fd, char *buf, int count)
 
 int MacWrite(int fd, const char *buf, int count)
 {
-	ParamBlockRec	param;
-	OSErr			theError;
-	int 			totalWritten = 0;							// How much of the buffer is left
-	long			blockSize;
-	short			vRef;
-
-	if( conversionBuffer == NULL )
-	{
-		conversionBuffer = NewPtr( WRITE_BUFFER_SIZE );
-		if( conversionBuffer == NULL || MemError() )
-		{
-			return -1;
-		}
-	}
-
-	if ((fd == 1) || (fd == 2)) 						// If it's 1/stdout or 2/stdtheError
-	{
-		if (InstallConsole(fd) == 0) 
-		{
-			__console_exit = RemoveConsole;
-			fflush(stdin);
-			
-			while( totalWritten < count )							// While there's more to write
-			{
-				if( totalWritten + WRITE_BUFFER_SIZE < count )		// If there's a whole buffer's worth
-					blockSize = WRITE_BUFFER_SIZE;					// Set the block size to the buffer size
-				else												// Otherwise
-					blockSize = count - totalWritten;				// Set the exact size
-				BlockMove( &( buf[ totalWritten ]), conversionBuffer, blockSize ); // Copy the block to the buffer
-				n2r( conversionBuffer, blockSize );
-				WriteCharsToConsole((char *)conversionBuffer, blockSize);
-				
-				totalWritten += blockSize;
-			}
-			
-		} 
-		else
-			count =  -1;
-		
-		return count;											// Naughty but what WriteChars.. does anyway
-	}
+	int result;
+	Boolean isText;
 	
-	if( FileIsText( fd ) )										// If it's a text file it needs translation
-	{
-		while( totalWritten < count )							// While there's more to write
-		{
-			if( count - totalWritten > WRITE_BUFFER_SIZE )		// If there's a whole buffer's worth
-				blockSize = WRITE_BUFFER_SIZE;					// Set the block size to the buffer size
-			else												// Otherwise
-				blockSize = count - totalWritten;				// Set the exact size
-			BlockMove( &( buf[ totalWritten ]), conversionBuffer, blockSize ); // Copy the block to the buffer
-			n2r( conversionBuffer, blockSize );					// Translate newlines
-			
-			theError = ReallyWrite( fd, conversionBuffer, &blockSize );
-
-			if (theError != noErr)								// If there's an error
-			{
-				FileError( fd );
-				fd = -1;								// Set the errno
-				break;											// And don't try again!
-			}
-			
-			totalWritten += blockSize;					// Move along
-		}
-	}
-	else														// Otherwise, it's a binary file. Don't translate
-	{
-		totalWritten = count;
-		theError = ReallyWrite( fd, buf, &totalWritten );
-	}
-	finally:
-	return (theError == noErr ? totalWritten : -1);			// Return the result
-}
-
-// ReallyWrite
-
-OSErr ReallyWrite( int fd, Ptr buffer, long * amount )
-{
-	short vRef;
-	OSErr theError;
+	assert( fd >= 0 );
+	assert( buf != NULL );
+	assert(  count > 0 );
 	
-	theError = FSWrite( fd, amount, buffer );
-	if (theError != noErr)									// If there's an error
-	{
-		return theError;
-	}
+	isText = FileIsText(fd);
 	
-	GetVRefNum( fd, &vRef );			// get file vRefNum
-	theError = FlushVol( NULL, vRef );
-	if (theError != noErr)									// If there's an error
+	assert( (isText == FALSE) || (isText == TRUE) );
+	
+	if( isText )
 	{
-		return theError;
+		n2r( buf, count );
+		result = write( fd, buf, count );
+		r2n( buf, count );	// Must be a better way
+	}
+	else
+	{
+		result = write( fd, buf, count );
 	}
 
-	return theError;
+	FileError( result );
+	
+	return result;
 }
 
 // select
@@ -431,35 +241,26 @@ int select( int nfds, struct fd_set *readfds, struct fd_set *write_fds, struct f
 	return 1;
 }
 
-// FindUnusedFileStatus
-// Get an unused file status record
-
-int FindUnusedFileStatus( void )
-{
-	int fileNum = 0;
-	
-	while( fileNum < MAX_FILES )
-	{
-		if( __fileIsText[ fileNum ].fileNum == -1 )
-			break;
-		fileNum++;
-	}
-	
-	return fileNum;
-}
-
 // FileIsText
 // Checks whether a file is text
 
 Boolean FileIsText( int fd )
 {
-	int fileNum = 0;
+	int i;
 	
-	while( fileNum < MAX_FILES )
+	assert( fd >= 0 );
+	
+	if( fd < 3 )		/* If it's one of the standard streams */
 	{
-		if( __fileIsText[ fileNum ].fileNum == fd )
-			return __fileIsText[ fileNum ].isText;
-		fileNum++;
+		return TRUE;	/* It's text */
+	}
+	
+	for( i = 0; i < MAX_FILES; i++ )
+	{
+		if( __fileIsBinary[ i ] == fd )
+		{
+			return FALSE;
+		}
 	}
 	
 	return TRUE;
@@ -470,7 +271,10 @@ Boolean FileIsText( int fd )
 // Convert newlines to returns
 
 void n2r( char * buffer, int count)
-{                                                                                    
+{                                  
+	assert( buffer != 0 );
+	assert( count > 0 );
+                                                  
    	while (count--)                                    
 	{      
 		switch( *buffer )
@@ -489,7 +293,10 @@ void n2r( char * buffer, int count)
 // Convert returns to newlines
 
 void r2n( char * buffer, int count)  
-{                                                      
+{                                   
+	assert( buffer != 0 );
+	assert( count > 0 );    
+	               
 	while (count--)                                    
 	{      
 		switch( *buffer )
@@ -508,7 +315,7 @@ void r2n( char * buffer, int count)
 // Opens the data fork of the app.
 // On PowerMac, the app MUST have its code in the resource fork!
 
-long OpenApplicationAsFile( void )
+short OpenApplicationAsFile( void )
 {
 	OSErr 				err;
 	short 				fd;
@@ -530,7 +337,7 @@ long OpenApplicationAsFile( void )
 		return -1;
 	}
 
-	err = FSpOpenDF(	&spec, fsCurPerm, &fd);
+	err = FSpOpenDF( &spec, fsCurPerm, &fd );
 	if( err != noErr )
 	{
 		return -1;
@@ -541,26 +348,77 @@ long OpenApplicationAsFile( void )
 	else
 		gD2c = FALSE;
 	
-	return (short)fd;
+	return fd;
 }
 
 // IsDUFile
 
-Boolean IsDUFile( Str255 name )
+Boolean IsDUFile( const char * name )
 {
-	int end = name[ 0 ];
+	int end = strlen( name );
 	
 	if( end < 4 )
 		return FALSE;
 
-	if(	name[ end-2 ] != '.' )
+	if(	name[ end-3 ] != '.' )
 		return FALSE;	
-	if(	name[ end-1 ] != 'd' )
+	if(	name[ end-2 ] != 'd' )
 		return FALSE;
-	if(	name[ end ] != 'u' )
+	if(	name[ end-1 ] != 'u' )
 		return FALSE;
 
 	return TRUE;
+}
+
+// IsDBCFile
+
+Boolean IsDBCFile( const char * name )
+{
+	int end = strlen( name );
+	
+	if( end < 5 )
+		return FALSE;
+
+	if(	name[ end-4 ] != '.' )
+		return FALSE;	
+	if(	name[ end-3 ] != 'd' )
+		return FALSE;
+	if(	name[ end-2 ] != 'b' )
+		return FALSE;
+	if(	name[ end-1 ] != 'c' )
+		return FALSE;
+
+	return TRUE;
+}
+
+
+void SetFileText( const char * path, int fd, int flags )
+{
+	int index = 0;
+
+	assert( path != 0 );
+	assert( fd >= 0 );
+
+    if( fd >= 0 )									// If opened ok
+	{
+		for( index = 0; index < MAX_FILES; index++ )
+		{
+			if( __fileIsBinary[ index ] == 0 )
+				break;
+		}
+		
+		if( index < MAX_FILES )
+		{
+			// hack for compiler binary file types
+			if(/*IsDBCFile( path ) ||*/ IsDUFile( path ) )
+				__fileIsBinary[ index ] = fd;
+			else
+			{
+				if( flags & O_BINARY )
+					__fileIsBinary[ index ] = fd;	// Set flag
+		    }
+		}	
+	}
 }
 
 // Error translation
@@ -568,49 +426,55 @@ Boolean IsDUFile( Str255 name )
 
 void FileError( int err )
 {
-	switch (err) {
-	case noErr:
-		errno = 0;
-		break;
-	case bdNamErr:
-		errno = (ENAMETOOLONG);
-		break;
-	case afpObjectTypeErr:
-		errno = (ENOTDIR);
-		break;
-	case fnfErr:
-	case dirNFErr:
-		errno = (ENOENT);
-		break;
-	case dupFNErr:
-		errno = (EEXIST);
-		break;
-	case dirFulErr:
-	case dskFulErr:
-		errno = (ENOSPC);
-		break;
-	/*case fBsyErr:
-		errno = (EBUSY);
-		break;*/
-	case tmfoErr:
-		errno =  (ENFILE);
-		break;
-	case fLckdErr:
-	case permErr:
-	case afpAccessDenied:
-		errno =  (EACCES);
-		break;
-	case wPrErr:
-	case vLckdErr:
-		errno =  (EROFS);
-		break;
-	case badMovErr:
-		errno =  (EINVAL);
-		break;
-	/*case diffVolErr:
-		errno =  (EXDEV);
-		break;*/
-	default:
-		errno =  (EINVAL);
+	int dummy;
+	
+	if( err == -1 )
+	{
+		switch (errno) 
+		{
+			case noErr:
+				errno = 0;
+				break;
+			case bdNamErr:
+				errno = (ENAMETOOLONG);
+				break;
+			case afpObjectTypeErr:
+				errno = (ENOTDIR);
+				break;
+			case fnfErr:
+			case dirNFErr:
+				errno = (ENOENT);
+				break;
+			case dupFNErr:
+				errno = (EEXIST);
+				break;
+			case dirFulErr:
+			case dskFulErr:
+				errno = (ENOSPC);
+				break;
+			/*case fBsyErr:
+				errno = (EBUSY);
+				break;*/
+			case tmfoErr:
+				errno =  (ENFILE);
+				break;
+			case fLckdErr:
+			case permErr:
+			case afpAccessDenied:
+				errno =  (EACCES);
+				break;
+			case wPrErr:
+			case vLckdErr:
+				errno =  (EROFS);
+				break;
+			case badMovErr:
+				errno =  (EINVAL);
+				break;
+			/*case diffVolErr:
+				errno =  (EXDEV);
+				break;*/
+			default:
+				errno = (EINVAL);
+		}
 	}
 }
