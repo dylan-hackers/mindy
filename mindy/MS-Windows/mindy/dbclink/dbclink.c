b861844de9324cfd82637f6cdede0ae4a3132fd3
Brent Fulgham <bfulgham@debian.org>
2001-08-09 20:07:36 +0000

    Updates for Win32 Native build

--- mindy/MS-Windows/mindy/dbclink/dbclink.c
+++ mindy/MS-Windows/mindy/dbclink/dbclink.c
@@ -8,98 +8,44 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <errno.h>
-
-const char* delim = "\r\n";
 
 int main(int argc, char *argv[])
 {
   char *outname = NULL;
-  char *dataname = NULL;
+  char *dbc_list = NULL;
   char *lib_file_names = NULL;
-  char *token;
-  char *call = NULL; 
-
   FILE *out = NULL;
-  FILE *data = NULL;
-
-  int done = 0;
-  int c;
-
-  long data_size = 0;
-  long items = 0;
-
-  struct stat st_data;
 
   // Check arguments
   if (argc < 3)
   {
-	  printf("dbclink [library name] [file of dbc filenames]");
+	  printf("dbclink [library name] [dbc filenames]");
 	  exit(1);
   }
 
   outname = *++argv;
-  dataname = *++argv;
-
-  // Get size of data list
-  if ( stat(dataname, &st_data) )
-  {
-	  printf("Error reading data file: %d", errno);
-	  exit(-1);
-  }
-
-  // Open data file and library output file
   out = fopen(outname, "wb");
-  data = fopen(dataname, "r");
-
-  // Allocate our buffer
-  data_size = st_data.st_size;
-  lib_file_names = (char*)malloc(data_size * sizeof(char));
-  if (lib_file_names == NULL)
+  
+  while (dbc_list = *++argv)
   {
-	  printf("Error allocating file memory: %d", errno);
-	  exit(-2);
-  }
+    int c = 0;
+    FILE *f = NULL;
 
-  // Read data list
-  items = fread(lib_file_names, sizeof(char), data_size, data);
-  if (items == 0)
-  {
-	  printf("Error reading data file: %d", errno);
-	  exit(-3);
-  }
-  lib_file_names[items] = EOF;
+    f = fopen(dbc_list, "rb");
+    if (f == NULL)
+    {
+       printf("Failed to open %s\r\n", dbc_list);
+       exit(2);
+    }
 
-  fclose(data);
-
-  //
-  //  For each entry in the data file, read the library data
-  //  and append it to the library file.
-  //
-  printf("Starting process...\r\n");
-  call = &lib_file_names[0];
-  while (token = strtok(call, delim))
-  {
-	  FILE *f = NULL;
-
-	  if (token[0] == EOF)
-		  break;
-
-	  f = fopen(token, "rb");
-
-	  printf("Processing library %s...\r\n", token);	  
-	  while ((c = fgetc(f)) != EOF) 
-	  {
-		  fputc(c, out);
-	  }
-	  fclose(f);
-	  call = NULL;
+    printf("Processing library %s...\r\n", dbc_list);	  
+    while ((c = fgetc(f)) != EOF) 
+    {
+       fputc(c, out);
+    }
+    fclose(f);
   }
 
   fclose(out);
-  free((void*)lib_file_names);
   exit(0);
 }
