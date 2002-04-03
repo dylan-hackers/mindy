550e07a4bd06b2982464b8f9ef893bd1497f6911
Gabor Greif <ggreif@gmail.com>
2002-04-03 23:17:33 +0000

    added copyright

--- d2c/compiler/Macintosh/GwydionPanel.cpp
+++ d2c/compiler/Macintosh/GwydionPanel.cpp
@@ -1,6 +1,37 @@
 // File: GwydionPanel.cpp
+// RCS-header: $Header: /scm/cvs/src/d2c/compiler/Macintosh/GwydionPanel.cpp,v 1.2 2002/04/03 23:17:33 gabor Exp $
+// Purpose: implement the programmatic part of CW preference panel
+// Author: Gabor Greif <gabor@mac.com>
 // Todo:	AEErr and CWErr checks (throws)
 // 				HLock(curr) i not yet...
+// Status: needs some cleanup, some messages not yet supported
+
+//======================================================================
+//
+// Copyright (c) 2000, 2001, 2002  Gwydion Dylan Maintainers
+// All rights reserved.
+// 
+// Use and copying of this software and preparation of derivative
+// works based on this software are permitted, including commercial
+// use, provided that the following conditions are observed:
+// 
+// 1. This copyright notice must be retained in full on any copies
+//    and on appropriate parts of any derivative works.
+// 2. Documentation (paper or online) accompanying any system that
+//    incorporates this software, or any part of it, must acknowledge
+//    the contribution of the Gwydion Project at Carnegie Mellon
+//    University, and the Gwydion Dylan Maintainers.
+// 
+// This software is made available "as is".  Neither the authors nor
+// Carnegie Mellon University make any warranty about the software,
+// its performance, or its conformity to any specification.
+// 
+// Bug reports should be sent to <gd-bugs@gwydiondylan.org>; questions,
+// comments and suggestions are welcome at <gd-hackers@gwydiondylan.org>.
+// Also, see http://www.gwydiondylan.org/ for updates and documentation. 
+//
+//======================================================================
+
 
 #define CW_STRICT_DIALOGS 1
 
