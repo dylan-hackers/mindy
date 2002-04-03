938075e939bc26208d16b9953d1819df6ed24a39
Gabor Greif <ggreif@gmail.com>
2002-04-03 23:11:55 +0000

    updated to Pro6 level, added rcs header and appropriate copyright notice

--- d2c/compiler/Macintosh/GwydionPanel.r
+++ d2c/compiler/Macintosh/GwydionPanel.r
@@ -1,7 +1,33 @@
 // File: GwydionPanel.r
-// Purpose: define necessary resources for panel
-// Author: Gabor Greif
-// Copyright: © 2000 Gabor Greif, all rights reserved.
+// Purpose: define necessary resources for CW preference panel
+// RCS-header: $Header: /scm/cvs/src/d2c/compiler/Macintosh/GwydionPanel.r,v 1.2 2002/04/03 23:11:55 gabor Exp $
+// Author: Gabor Greif <gabor@mac.com>
+
+//======================================================================
+//
+// Copyright (c) 2000, 2001, 2002  Gwydion Dylan Maintainers
+// All rights reserved.
+// 
+// Use and copying of this software and preparation of derivative
+// works based on this software are permitted, including commercial
+// use, provided that the following conditions are observed:
+// 
+// 1. This copyright notice must be retained in full on any copies
+//    and on appropriate parts of any derivative works.
+// 2. Documentation (paper or online) accompanying any system that
+//    incorporates this software, or any part of it, must acknowledge
+//    the contribution of the Gwydion Project at Carnegie Mellon
+//    University, and the Gwydion Dylan Maintainers.
+// 
+// This software is made available "as is".  Neither the authors nor
+// Carnegie Mellon University make any warranty about the software,
+// its performance, or its conformity to any specification.
+// 
+// Bug reports should be sent to <gd-bugs@gwydiondylan.org>; questions,
+// comments and suggestions are welcome at <gd-hackers@gwydiondylan.org>.
+// Also, see http://www.gwydiondylan.org/ for updates and documentation. 
+//
+//======================================================================
 
 #include "Types.r"
 #include "CWPlugins.r"
@@ -11,37 +37,38 @@
 #define NameForIDE "Dylan Compiler"
 #define NameForUSER "Dylan Language"
 
+#define VERSION7API 7
+
 resource 'vers' (1, NameForIDE" Vers")
 {
-	2,
-	0x33,
+	CodebaseMajor,
+	CodebaseMinor,
 	DevelopmentStage,
 	DevelopmentVersion,
 	verUS,
-	"2.3.3" DevelopmentState,
-	"v2.3.3" DevelopmentState ", a Gwydion Dylanª component"
+	CodebaseVersion DevelopmentState,
+	"v" CodebaseVersion DevelopmentState ", a Gwydion Dylanª component"
 };
 
 resource 'vers' (2, NameForIDE" Prod")
 {
-	2,
-	0x33,
+	CodebaseMajor,
+	CodebaseMinor,
 	DevelopmentStage,
 	DevelopmentVersion,
 	verUS,
-	"2.3.3" DevelopmentState,
-	"© 2000, Gwydion Dylan maintainers"
+	CodebaseVersion DevelopmentState,
+	"© " CopyrightYears ", Gwydion Dylan maintainers"
 };
 
 resource 'Flag' (128, NameForIDE)
 {
-	kCurrentResourceVersion,
 	Panel {
-		VERSION7API {
+			VERSION7API,
 			usesStrictAPI,
 			doesntSupportByteSwapping,
-			supportsReadWrite,
-			reserved,
+			supportsTextSettings,
+			doesntUseCrossPlatformAPI,
 			reserved,
 			reserved,
 			reserved,
@@ -71,11 +98,10 @@ resource 'Flag' (128, NameForIDE)
 			reserved,
 			reserved,
 			FrontEnd,
-			Pro5,
+			newestAPIVersion,
 			1,	// preference data version
-			target
+			panelScopeTarget
 		}
-	}
 };
 
 resource 'STR ' (128, NameForIDE" Name")
