f9020e0c485b5e79fbea841a8a0b2e2b74f5aca8
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-09-29 11:49:06 +0700

    Enable tests.
    * CMakeLists.txt
      (ENABLE_TESTING): Execute here.
      (tests): Include additional subdirectory.
    
    * cmake/AddDylan.cmake
      (ADD_DYLAN_TEST): New macro for adding tests.
    
    * tests/CMakeLists.txt: Build tests. This leaves three
      tests commented out for now as they do not work.

--- cmake/AddDylan.cmake
+++ cmake/AddDylan.cmake
@@ -27,3 +27,10 @@ ENDMACRO()
 MACRO(ADD_DYLAN_LIBRARY library_name)
   ADD_DYLAN_PROJECT(${library_name} "${CMAKE_BINARY_DIR}/lib/")
 ENDMACRO()
+
+MACRO(ADD_DYLAN_TEST test_name)
+  ADD_DYLAN_PROJECT(${test_name} "${CMAKE_BINARY_DIR}/test-bin/")
+  ADD_TEST(NAME "test-${test_name}"
+           WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/lib/"
+           COMMAND mindy -f ${DYLAN_${test_name}_OUTPUT})
+ENDMACRO()
