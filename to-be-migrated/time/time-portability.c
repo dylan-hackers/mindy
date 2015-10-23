5f4e3b787237388d906cbd7bbfb9547b68b78735
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-10-23 19:59:03 +0700

    Clean up time-portability.[ch]
    * Remove unused cygwin daylight block.
    * Move include of <time.h> to C file.
    * Add include guards to the header file.

--- to-be-migrated/time/time-portability.c
+++ to-be-migrated/time/time-portability.c
@@ -1,6 +1,8 @@
 #include "time-portability.h"
 #include "config.h"
 
+#include <time.h>
+
 #if defined(HAVE_DAYLIGHT)
 int
 my_timezone(void)
@@ -28,18 +30,6 @@ my_daylight(void)
   time_t now;
   time(&now);
   return -(localtime(&now)->tm_gmtoff);
-}
-#elif defined(HAVE_CYGNUS_DAYLIGHT)
-int
-my_timezone(void)
-{
-  return _timezone;
-}
-
-int
-my_daylight(void)
-{
-  return _daylight;
 }
 #elif defined(_WIN32)
 #include <Windows.h>
--- to-be-migrated/time/time-portability.h
+++ to-be-migrated/time/time-portability.h
@@ -1,9 +1,7 @@
-#if defined(__CYGWIN__)
-#define __extension__
-#include <machine/types.h>
-#endif
-#include <time.h>
+#ifndef MINDY_TIME_PORTABILITY_H_
+#define MINDY_TIME_PORTABILITY_H_
 
 extern int my_timezone(void);
 extern int my_daylight(void);
 
+#endif
