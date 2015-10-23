a45aa76b249e9394a61d00b61db5c41b91062eb6
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-10-23 15:46:04 +0600

    [windows] Initial commit of shared library functions.
    These aren't tested yet since mindy doesn't run successfully on
    Windows yet.

--- mindy/interpreter/shl_windows.c
+++ mindy/interpreter/shl_windows.c
@@ -1,1 +1,28 @@
+/*
+** This module provides a simple shared library loading interface
+** for Windows based machines.
+*/
 #include "shl.h"
+#include <Windows.h>
+
+shl_t shl_load (const char *path)
+{
+	return LoadLibraryA(path);
+}
+
+int shl_findsym (shl_t *handle, const char *sym, void **value)
+{
+    static void *self_handle = 0;
+
+    if (*handle == 0) abort();
+    if (*handle == PROG_HANDLE) {
+        if (self_handle == 0) {
+            self_handle = GetModuleHandle(NULL);
+        }
+        handle = &self_handle;
+    }
+
+    *value = GetProcAddress(*handle, (char *)sym);
+
+    return *value == NULL;
+}
